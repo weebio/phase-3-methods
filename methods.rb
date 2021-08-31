af111c793d6cbff63218a897d675d78b64d840f5
Evaristo <bubbali2002@gmail.com>
2021-08-31 11:23:23 -0500

    commit tree 59fec0a3b483e76f155f65713aca65ac441271f3

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add (num1, num2)
+    return num1 + num2 
+end
+
+
+def halve (num3)
+ if num3.is_a? Integer
+    num3 / 2
+ end
+end
+
